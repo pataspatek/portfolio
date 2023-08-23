7717826a08ea27b1f32b44910b91fc4082967710
pataspatek <patrikpatek3@gmail.com>
2023-08-23 21:46:17 +0200

    Navbar in plain CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,31 @@
 import { Link } from "react-router-dom";
 import React from "react";
 
+import '../styles/header.css'
+
 function Header() {
     return (
-        <div className="container">
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" fill="currentColor" className="bi me-2" viewBox="0 0 16 16">
-                        <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
-                    </svg>
-                    <span className="fs-4">Patrik Pátek</span>
-                </Link>
-
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/features" className="nav-link">Projects</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/pricing" className="nav-link">Pricing</Link>
-                    </li>
-                </ul>
-
-            </header>
+        <div className="header-container">
+            <Link className="logo-link">
+                <svg className="logo-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+                </svg>
+                <span className="logo-text">Patrik Pátek</span>
+            </Link>
+            <ul className="nav-links">
+                <li className="nav-item">
+                    <Link className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link">Projects</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link">Contact</Link>
+                </li>
+            </ul>
         </div>
     );
 }
