1e6ade7856496669881edd308b76efedd161f747
pataspatek <patrikpatek3@gmail.com>
2023-09-06 20:05:24 +0200

    activities links added

--- frontend/assets/ActivitiesList.jsx
+++ frontend/assets/ActivitiesList.jsx
@@ -6,19 +6,19 @@ const activities = [
     {
         name: 'Programming',
         description: 'Embark on a coding adventure, crafting digital marvels, and shaping the future with your innovative software creations in the vast realm of programming.',
-        imgPath: '/assets/images/programming.png',
+        linkTo: "/projects",
         iconPath: "M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"
     },
     {
         name: 'Writing',
         description: 'Enter a world of words and stories, weaving narratives that resonate. Writing is your canvas for imagination, where words paint emotions and ideas come to life.',
-        imgPath: '/assets/images/programming.png',
+        linkTo: "/writing",
         iconPath: "M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
     },
     {
         name: 'Courses',
         description: 'Fuel your lifelong learning journey with a wide array of courses. Expand your horizons, gain expertise, and embrace the endless possibilities of self-improvement',
-        imgPath: '/assets/images/programming.png',
+        linkTo: "/courses",
         iconPath: "M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
     },
     // Add more activities as needed
